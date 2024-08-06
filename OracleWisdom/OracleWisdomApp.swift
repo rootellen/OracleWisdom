868ee048522c9ffb1c0b7b40f423e8c02343cb87
Ruth <ruthellennds@gmail.com>
2024-08-06 20:10:32 -0300

    Initial Commit

---
+++ OracleWisdom/OracleWisdomApp.swift
@@ -0,0 +1,17 @@
+//
+//  OracleWisdomApp.swift
+//  OracleWisdom
+//
+//  Created by Ruth Ellen on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct OracleWisdomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
